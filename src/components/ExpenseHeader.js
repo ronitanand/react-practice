6c44fc786bac9a987c8f94b716f29f4c4fde96e3
Ronit Anand <rntanand@gmail.com>
2022-03-26 19:34:35 +0530

    adding

---
+++ src/components/ExpenseHeader.js
@@ -0,0 +1,14 @@
+import './ExpenseHeader.css';
+
+function ExpenseHeader(){
+
+    return(
+        <div className='expense-header'>
+            Expense Tracker
+        </div>
+    )
+
+
+}
+
+export default ExpenseHeader;
